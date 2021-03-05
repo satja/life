a1ad56dc2ee16b361cb36b3aca8b6374daedad51
Adrian Satja Kurdija <askurdija@gmail.com>
2021-03-05 07:36:19 +0100

    modify life module

--- live.py
+++ live.py
@@ -1,4 +1,5 @@
 import random
+from mind import think_about
 
 def wake_up():
     while still_sleepy:
@@ -25,34 +26,49 @@ def sleep():
             insomnia = True
 
 def think_about_stuff():
-    thought = random.choice(subconsciousness + consciousness)
+    thought = random.choice(subconsciousness + consciousness + ['anna-maria'])
     while have_nothing_to_do:
-        think(thought)
+        think_about(thought)
         if random.randrange(10) > 0:
             thought = random.choice(related(thought))
         else:
-            thought = random.choice(subconsciousness + consciousness)
+            thought = random.choice(subconsciousness + consciousness + ['anna-maria'])
 
 def live():
     while len(things_you_can_do) > 0:
-        think_about_stuff()
+        think_about('anna-maria')
 
         if len(things_you_really_must_do) > 0:
             if random.randrange(2) == 0:
-                thing_to_do = random.choice(things_you_really_must_do)
-                do(thing_to_do)
+                thing = random.choice(things_you_really_must_do)
+                do(thing)
+
+        think_about('♥ ')
 
         if len(things_you_should_do) > 0:
             if random.randrange(3) == 0:
-                thing_to_do = random.choice(things_you_should_do)
-                do(thing_to_do)
+                thing = random.choice(things_you_should_do)
+                do(thing)
+
+        think_about('anna-maria')
 
         if len(things_you_should_never_do) > 0:
             if random.randrange(4) == 0:
-                thing_to_do = random.choice(things_you_should_never_do)
-                do(thing_to_do)
+                thing = random.choice(things_you_should_never_do)
+                do(thing)
 
-        thing_to_do = random.choice(things_you_can_do)
-        do(thing_to_do)
+        think_about('♥ ')
+
+        thing = random.choice(
+                things_to_do_with('anna-maria') + things_to_do_for('anna-maria'))
+        do(thing)
         
-        think_about_stuff()
+        think_about('anna-maria')
+
+    # rename this function
+    live.__name__[1] = 'o'
+
+
+if True:
+    # rename the whole module live.py
+    __name__[1] = 'o'
